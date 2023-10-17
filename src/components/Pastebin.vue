26bfc0a79a611d861d53ef503584215ba4f286fc
Merlyn Allen <me@xiaorui.li>
2023-10-17 14:57:26 +0800

    Fixed base64 -> utf-8 issues

--- src/components/Pastebin.vue
+++ src/components/Pastebin.vue
@@ -2,75 +2,78 @@
 <script>
 import { ref } from "vue";
 export default {
-    name: 'Pastebin',
-    setup() {
-        const url = new URL(window.location.href);
-        const encodedPaste = url.pathname.substring(1);
-        const paste = encodedPaste; // Do not decode, decode in client
-        const decodedPaste = decodeURIComponent(encodeURI(atob(paste)));
-
-        const msg = ref(decodedPaste);
-        const showmsg = ref(false);
-
-        return {
-            msg,
-            showmsg
-        }
-    }, 
-    mounted() {
-
-        navigator.clipboard.writeText(self.msg);
-        this.showMessage();
-    },
-    methods:{
-        showMessage() {
-            this.showmsg = true;
-            setTimeout(() => {
-                this.showmsg = false;
-            }, 3000);
-        },
-        onclick() {
-            navigator.clipboard.writeText(self.msg);
-            this.showMessage();
-        }
-
+  name: "Pastebin",
+  setup() {
+    function base64ToBytes(base64) {
+      const binString = atob(base64);
+      return new TextDecoder().decode(
+        Uint8Array.from(binString, (m) => m.codePointAt(0))
+      );
     }
-}
+    const url = new URL(window.location.href);
+    const encodedPaste = url.pathname.substring(1);
+    const paste = encodedPaste; // Do not decode, decode in client
+    const decodedPaste = base64ToBytes(paste);
+
+    const msg = ref(decodedPaste);
+    const showmsg = ref(false);
+
+    return {
+      msg,
+      showmsg,
+    };
+  },
+  mounted() {
+    navigator.clipboard.writeText(self.msg);
+    this.showMessage();
+  },
+  methods: {
+    showMessage() {
+      this.showmsg = true;
+      setTimeout(() => {
+        this.showmsg = false;
+      }, 3000);
+    },
+    onclick() {
+      navigator.clipboard.writeText(self.msg);
+      this.showMessage();
+    },
+  },
+};
 </script>
 <template>
-    <div class="container">
-        <textarea class="textbox" :value="msg" readonly></textarea>
-        <input type="button" value="COPY" @click="onclick">
-        <span class="msgbox" :class='{show: showmsg}'>Copied to clipboard!</span> 
-    </div>
+  <div class="container">
+    <textarea class="textbox" :value="msg" readonly></textarea>
+    <input type="button" value="COPY" @click="onclick" />
+    <span class="msgbox" :class="{ show: showmsg }">Copied to clipboard!</span>
+  </div>
 </template>
 
 <style>
 textarea {
-    resize: none;
+  resize: none;
 }
 .msgbox {
-    visibility: hidden;
-    opacity: 0;
-    position: absolute;
-    content: 'Copied to clipboard!';
-    margin: auto 30%;
-    left: 0;
-    right: 0;
-    top: 80%;
-    border-style: solid;
-    border-radius: 10px;
-    border-width: 1px;
-    border-color: var(--placeholder);
-    text-align: center;
-    padding: 10px 0px;
-    background-color: var(--blue);
-    transition: opacity 0.2s ease-in-out, visibility 0.2s ;
+  visibility: hidden;
+  opacity: 0;
+  position: absolute;
+  content: "Copied to clipboard!";
+  margin: auto 30%;
+  left: 0;
+  right: 0;
+  top: 80%;
+  border-style: solid;
+  border-radius: 10px;
+  border-width: 1px;
+  border-color: var(--placeholder);
+  text-align: center;
+  padding: 10px 0px;
+  background-color: var(--blue);
+  transition: opacity 0.2s ease-in-out, visibility 0.2s;
 }
 .show {
-    visibility: visible;
-    opacity: 1;
-    transition: opacity 0.2s ease-in-out;
+  visibility: visible;
+  opacity: 1;
+  transition: opacity 0.2s ease-in-out;
 }
-
 </style>
